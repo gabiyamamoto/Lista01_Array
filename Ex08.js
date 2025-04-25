94657a343b24502bcfcef13ab495774ea6447fe3
Gabi Emi <gabiemi2021@gmail.com>
2025-04-25 11:09:33 -0300

    8 - Só os maiores de idade

---
+++ Ex08.js
@@ -0,0 +1,7 @@
+let idades = [12, 17, 30];
+
+for (let i = 0; i < idades.length; i++) {
+    if (idades[i] >= 18) {
+        console.log(idades[i]);
+    }
+}
---
+++ Ex10.js
@@ -0,0 +1,7 @@
+let jogos = [`Honkai Star Rail`, `Roblox`, `Minecraft`];
+let jogoFavorito = `Roblox`;
+
+for (let i = 0; i < jogos.length; i++);
+if (jogoFavorito === jogos[i]) {
+    console.log (`🏆 ${i + 1} ° lugar: ${jogos[i]}`);
+}
