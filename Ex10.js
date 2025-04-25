03ac830d0622d8d9b59095b86d2e95fdd14edd65
Gabi Emi <gabiemi2021@gmail.com>
2025-04-25 11:38:36 -0300

    10 - Desafio Bônus - Ranking interativo

--- Ex10.js
+++ Ex10.js
@@ -4,5 +4,5 @@ let jogoFavorito = `Roblox`;
 for (let i = 0; i < jogos.length; i++) {
     if (jogoFavorito == jogos[i]) {
     console.log (`Jogo favorito é 🏆 ${jogos[i]}: ${i + 1}° lugar!`);
-} else
+}
 }
