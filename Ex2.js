000fb2307d6a30f4a99365e114291372e3257c8b
Gabi Emi <gabiemi2021@gmail.com>
2025-04-25 11:45:30 -0300

    2 - Mostre sua playlist favorita

--- Ex2.js
+++ Ex2.js
@@ -1,6 +1,6 @@
 let playlist = [`You give love a bad name`, `Use your heart (interlude)`, `Shut up mom is calling`, `Mind games`, `Harleys in Hawaii`];
 let quantidade = playlist.length;
 
-for (let i = 0; i < quantidade; i++){
+for (let i = 0; i < quantidade; i++) {
     console.log(playlist[i]);
 }
