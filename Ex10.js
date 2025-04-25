166978022560b3e031ad9ec6c0250eabb9c8f76a
Gabi Emi <gabiemi2021@gmail.com>
2025-04-25 11:13:19 -0300

    10 - Desafio Bônus - Ranking Interativo

--- Ex10.js
+++ Ex10.js
@@ -1,7 +1,8 @@
 let jogos = [`Honkai Star Rail`, `Roblox`, `Minecraft`];
-let jogoFavorito = `Roblox`;
+let jogoFavorito = `Honkai Star Rail`;
 
-for (let i = 0; i < jogos.length; i++);
-if (jogoFavorito === jogos[i]) {
+for (let i = 0; i < jogos.length; i++) {
+    if (jogoFavorito === jogos[i]) {
     console.log (`🏆 ${i + 1} ° lugar: ${jogos[i]}`);
+}
 }
